3c92c0e314edf4c2bb9784c595554ecbdab9c322
Jonathan Neidel <github@jneidel.com>
2019-01-25 13:13:28 +0100

    Update env vars, their description, reading them

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -9,20 +9,23 @@ if ( existsSync( ".env" ) ) {
   dotenv( { path: ".env.example" } ); // Fallback if no own .env file exists
 }
 
-/* if ( !process.env.SESSION_SECRET ) {
-  throw new Error( "No client secret. Set SESSION_SECRET environment variable." );
-  process.exit();
-}
+[ // Required env variables
+  { var: "DEFAULT_KEYID", msg: "default keyid" },
+  { var: "MYSQL_USER", msg: "mysql username" },
+  { var: "MYSQL_PASS", msg: "mysql password for the corresponding user" },
+].forEach( x => {
+  if ( !process.env[x.var] ) {
+    throw new Error( `No ${x.msg} given. Set ${x.var} environment variable.` );
+    process.exit();
+  }
+})
 
-if ( !process.env.MONGODB_URI ) {
-  throw new Error( "No mongo connection string. Set MONGODB_URI environment variable." );
-  process.exit();
-}
-
-const MONGODB_URI: string = process.env.MONGODB_URI;
-*/
 export const ENVIRONMENT: string = process.env.NODE_ENV || "development";
 export const SESSION_SECRET: string = process.env.SESSION_SECRET || "5519888571";
 export const PORT: number = Number( process.env.PORT ) || 8000;
-export const DEFAULT_KEYID: string = process.env.DEFAULT_KEYID || "B29E6A7A7DFD16FA";
+export const DEFAULT_KEYID: string | undefined = process.env.DEFAULT_KEYID;
+export const MYSQL_HOST: string = process.env.MYSQL_HOST || "127.0.0.1";
+export const MYSQL_USER: string | undefined = process.env.MYSQL_USER;
+export const MYSQL_PASS: string | undefined = process.env.MYSQL_PASS;
+export const MYSQL_DB: string = process.env.MYSQL_DB || "lock-me-out";
 
