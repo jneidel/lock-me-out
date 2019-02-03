592c8a17b5669548d98995b2122e9c0ee8f04284
Jonathan Neidel <github@jneidel.com>
2019-02-03 20:05:45 +0100

    Add user model

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,15 +4,17 @@ import { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, MYSQL_PORT } from "../uti
 const db = new SequelizeClass( `mysql://${MYSQL_USER}:${MYSQL_PASS}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB}`,
   {
     operatorsAliases: false,
-    timezone: "+01:00"
-  } )
+    timezone        : "+01:00",
+  } );
 
 // Import models
-Array.from( [ "Item" ] )
-  .forEach( model => {
-    const Model = db.import( model,  require( `./models/${model}` ) );
-    db[Model.name] = Model;
-  } );
+const Item = db.import( "Item", require( "./models/Item" ) );
+const User = db.import( "User", require( "./models/User" ) );
+
+User.hasMany( Item, { as: "Items" } ); // Get all user items with: user.getItems()
+
+db.Item = Item;
+db.User = User;
 
 export default db;
 export { Sequelize };
---
+++ src/db/models/User.ts
@@ -0,0 +1,33 @@
+import uuid from "uuid/v4";
+import reservedUsernames from "./reserved-usernames";
+import { DEFAULT_KEYID } from "../../util/secrets";
+
+export default function createItem( sequelize, DataTypes ) {
+  return sequelize.define( "User", {
+    id: {
+      type        : DataTypes.STRING,
+      defaultValue: uuid(),
+      primaryKey  : true,
+      allowNull   : false,
+      validate    : {
+        notIn: [ reservedUsernames ],
+      },
+    },
+    keyid: {
+      type        : DataTypes.STRING,
+      defaultValue: DEFAULT_KEYID,
+      allowNull   : false,
+      validate    : {
+        len: [ 8, 16 ],
+      },
+    },
+  }, {
+    tableName: "users",
+    indexes  : [
+      {
+        unique: true,
+        fields: [ "id" ],
+      },
+    ],
+  } );
+}
