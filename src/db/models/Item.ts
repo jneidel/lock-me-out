72bb287235ee6743483f028f185dc193fb3302f2
Jonathan Neidel <github@jneidel.com>
2019-02-03 15:48:23 +0100

    Convert Item model to sequalize

---
+++ src/db/models/Item.ts
@@ -0,0 +1,62 @@
+import uuid from "uuid/v4";
+import { DEFAULT_KEYID } from "../../util/secrets";
+
+/*
+ * Each encrypted password is described as an item
+ */
+
+export default function createItem( sequelize, DataTypes ) {
+  return sequelize.define( "Item", {
+    id: {
+      type: DataTypes.STRING,
+      defaultValue: uuid(),
+      primaryKey: true,
+      allowNull: false,
+      validate: {
+        isUUID: 4,
+      }
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    keyid : {
+      type: DataTypes.STRING,
+      defaultValue: DEFAULT_KEYID,
+      allowNull: false,
+      validate: {
+        len: [ 8, 16 ]
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      defaultValue: null,
+      allowNull: true,
+      get() {
+        const name = this.getDataValue( "name" );
+        const keyid = this.getDataValue( "keyid" );
+
+        return name === null ? keyid : name;
+      },
+      validate: {
+        notEmpty: true,
+      }
+    },
+    default: { // Will never be accessed directly
+      get() {
+        return DEFAULT_KEYID === this.getDataValue( "keyid" );
+      },
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    }
+  }, {
+    tableName: "items",
+    indexes: [
+      {
+        unique: true,
+        fields: [ "id" ]
+      },
+    ]
+  })
+}
+
