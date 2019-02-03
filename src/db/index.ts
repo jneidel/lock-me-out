cfd8a02b61644b628ce196e6f8584a7f7bb4411e
Jonathan Neidel <github@jneidel.com>
2019-02-03 15:49:26 +0100

    Add model import

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,19 @@
 import SequelizeClass, * as Sequelize from "sequelize";
 import { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, MYSQL_PORT } from "../util/secrets";
 
-export default new SequelizeClass( `mysql://${MYSQL_USER}:${MYSQL_PASS}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB}` )
-export { Sequelize }
+const db = new SequelizeClass( `mysql://${MYSQL_USER}:${MYSQL_PASS}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB}`,
+  {
+    operatorsAliases: false,
+    timezone: "+01:00"
+  } )
+
+// Import models
+Array.from( [ "Item" ] )
+  .forEach( model => {
+    const Model = db.import( model,  require( `./models/${model}` ) );
+    db[Model.name] = Model;
+  } );
+
+export default db;
+export { Sequelize };
 
