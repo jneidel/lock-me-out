c444890c6b4dcde9013483727eb9ab4b8dd0c510
Jonathan Neidel <github@jneidel.com>
2019-02-08 15:26:18 +0100

    Add basic /status page

--- src/public/js/status.js
+++ src/public/js/status.js
@@ -1,1 +1,12 @@
+const axios = require( "axios" );
 
+const submitButton = document.querySelector( "button" );
+const inputField = document.querySelector( "input" );
+
+submitButton.addEventListener( "click", async () => {
+  const value = inputField.value;
+
+  axios.post( "/api/get-status", { value } )
+    .then( res => res.data )
+    .then( data => console.log( data ) );
+} );
--- src/routes/view.ts
+++ src/routes/view.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 const router = Router();
 
-router.get( "/", ( req, res ) => res.render( "welcome", { title: "lock-me-out" } ) );
+router.get( "/", ( req, res ) => res.render( "welcome", { title: "Lock-Me-Out" } ) );
+router.get( "/new", ( req, res ) => res.render( "new", { title: "New lockout" } ) );
+router.get( "/status", ( req, res ) => res.render( "status", { title: "Status", defaultValue: "test" } ) );
 
 export default router;
 
