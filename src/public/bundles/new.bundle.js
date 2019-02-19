6554555a5597d2e4aada267aa96345ab31f7574b
Jonathan Neidel <github@jneidel.com>
2019-02-19 21:13:50 +0100

    Refactor input field styles

--- src/public/bundles/new.bundle.js
+++ src/public/bundles/new.bundle.js
@@ -1,4 +1,5 @@
 require( "../js/new" );
 
 require( "../scss/layout.scss" );
+require( "../scss/input.scss" );
 require( "../scss/new.scss" );
--- src/public/bundles/status.bundle.js
+++ src/public/bundles/status.bundle.js
@@ -1,4 +1,5 @@
 require( "../js/status" );
 
 require( "../scss/layout.scss" );
+require( "../scss/input.scss" );
 require( "../scss/status.scss" );
