167393122862e87a58185cb28275ec47c6b2567f
Jonathan Neidel <github@jneidel.com>
2019-02-19 21:06:33 +0100

    Remove authentication routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,11 +3,11 @@ const router = Router();
 
 router.get( "/", ( req, res ) => {
   req.flash( "error", "Access to the API denied." );
-  res.status( 403 ).redirect( "/login" );
+  res.status( 403 ).redirect( "/" );
 } );
 router.get( "/:anything", ( req, res ) => {
   req.flash( "error", "Access to the API denied." );
-  res.status( 403 ).redirect( "/login" );
+  res.status( 403 ).redirect( "/" );
 } );
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import viewRoutes from "./view";
 import apiRoutes from "./api";
-import authRoutes from "./auth";
 import * as errorRoutes from "./error";
 
 export default ( app, ENVIRONMENT: string ) => {
   app.use( "/", viewRoutes );
   app.use( "/api", apiRoutes );
-  app.use( "/", authRoutes );
 
   app.use( errorRoutes.notFound );
   app.use( errorRoutes.flashValidationErrors );
