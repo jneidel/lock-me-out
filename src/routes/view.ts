05560b8d360815e24cbf0eb0a0130415aa8a4d71
Jonathan Neidel <github@jneidel.com>
2019-02-20 16:16:00 +0100

    Add logged in user to header

--- src/routes/view.ts
+++ src/routes/view.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 const router = Router();
 
 router.get( "/", ( req, res ) => res.render( "welcome", { title: "Lock-Me-Out" } ) );
-router.get( "/new", ( req, res ) => res.render( "new", { title: "New lockout" } ) );
-router.get( "/status", ( req, res ) => res.render( "status", { title: "Status", defaultValue: "test" } ) );
+router.get( "/new", ( req, res ) => res.render( "new", { title: "New lockout", user: "test" } ) );
+router.get( "/status", ( req, res ) => res.render( "status", { title: "Status", user: "test" } ) );
 
 export default router;
 
