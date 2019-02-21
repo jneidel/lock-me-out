b2ac358562ffe7556f4dc228786decaa6d296f01
Jonathan Neidel <github@jneidel.com>
2019-02-21 16:39:30 +0100

    Add /new item redirects to /status for the item

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,17 @@
 import { Router } from "express";
 const router = Router();
 
+router.post( "/new-item", ( req, res ) => {
+  const data = req.body;
+
+  console.log( data )
+
+  const itemId = "123";
+
+  res.status( 200 ).redirect( `/status?item=${itemId}` );
+} );
+
+// Get redirect
 router.get( "/", ( req, res ) => {
   req.flash( "error", "Access to the API denied." );
   res.status( 403 ).redirect( "/" );
--- src/routes/view.ts
+++ src/routes/view.ts
@@ -3,7 +3,7 @@ const router = Router();
 
 router.get( "/", ( req, res ) => res.render( "welcome", { title: "Lock-Me-Out" } ) );
 router.get( "/new", ( req, res ) => res.render( "new", { title: "New lockout", user: "test" } ) );
-router.get( "/status", ( req, res ) => res.render( "status", { title: "Status", user: "test" } ) );
+router.get( "/status", ( req, res ) => res.render( "status", { title: "Status", user: "test", item: req.query.item } ) );
 
 export default router;
 
