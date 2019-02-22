49d60369321120c0f16cd42405b4a82758799f9c
Jonathan Neidel <github@jneidel.com>
2019-02-22 22:19:56 +0100

    Add gpg configuration

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,5 +1,5 @@
-import { config } from "lock-me-out-api";
-import { CONFIG_DIR } from "./secrets";
+import { config, gpg } from "lock-me-out-api";
+import { CONFIG_DIR, DEFAULT_KEYID, DEFAULT_PASSPHRASE, EXEC_SHELL } from "./secrets";
 
 /*
  * Setup configuration directory (where gpg keys and encrypted items are stored)
@@ -7,5 +7,16 @@ import { CONFIG_DIR } from "./secrets";
 export default function initializeConfig() {
   config.setConfigDir( CONFIG_DIR );
   config.createConfigDir();
+
+  gpg.configure( { configDir: CONFIG_DIR, keyid: DEFAULT_KEYID, passphrase: DEFAULT_PASSPHRASE, shell: EXEC_SHELL } );
+  gpg.version()
+    .then( version => {
+      if ( version === null || version[0] < 2 ) {
+        console.error( `GPG version too low, v2.x is required.
+  Current version: ${version}
+  See https://github.com/jneidel/lock-me-out for gpg 2.x install instructions.` )
+        process.exit();
+      }
+    } );
 }
 
