8fd16cf8d144bf326e3c97112432e22bbf471f49
Jonathan Neidel <github@jneidel.com>
2019-02-24 15:54:11 +0100

    Add mongo insertKeyid functions

--- src/db/mongodb/index.ts
+++ src/db/mongodb/index.ts
@@ -4,11 +4,9 @@ import User from "./models/User";
 type ItemData {
   date: Date,
   name: String,
-  //keyid: String,
   user: String,
 }
-
-export async function createItem( data: ItemData ) {
+export async function createItem( data: ItemData ): String {
   const item = new Item( data );
 
   return item.save() // Handle error in routes
@@ -17,12 +15,21 @@ export async function createItem( data: ItemData ) {
 
 type UserData {
   id: String,
-  //keyid: String,
 }
-
-export async function createUser( data: UserData ) {
+export async function createUser( data: UserData ): String {
   const user = new User( data );
 
   return user.save() // Handle error in routes
     .then( () => user.id );
 }
+
+async function insertKeyid( db: {}, id: String, keyid: String ) {
+  db.updateOne( { id }, { keyid } );
+}
+export async function insertItemKeyid( id: String, keyid: String ): void {
+  insertKeyid( Item, id, keyid );
+}
+export async function insertUserKeyid( id: String, keyid: String ): void {
+  insertKeyid( User, id, keyid );
+}
+
