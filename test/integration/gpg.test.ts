0ca15380cde76735d1e46205d93cf3e0f6f7ae36
Jonathan Neidel <github@jneidel.com>
2019-02-25 16:58:39 +0100

    Update tests

--- test/integration/gpg.test.ts
+++ test/integration/gpg.test.ts
@@ -1,12 +1,29 @@
-import { gpg } from "lock-me-out-api";
+import * as path from "path";
+import { sync as rmrf } from "rimraf";
+import { config, gpg } from "lock-me-out-api";
 
-const CONFIG_DIR = "test-config",
-  DEFAULT_KEYID = "73BC264DEF10208E14DDA442B008B7BBE95A59F2",
-  DEFAULT_PASSPHRASE = "123",
-  EXEC_SHELL = "/bin/bash";
+const CONFIG_DIR = "config-test";
+const EXEC_SHELL = "/bin/bash";
+const DEFAULT_PASSPHRASE = "123";
+let DEFAULT_KEYID = "";
 
-test( "configure gpg", () => {
+beforeAll( async () => {
+  // Remove previous config dir
+  const configPath = path.resolve( __dirname, "..", "..", CONFIG_DIR );
+  rmrf( configPath ); 
+
+  // Setup config dir
+  config.setConfigDir( CONFIG_DIR );
+  config.createConfigDir();
+
+  // Generate default key
   gpg.configure( { configDir: CONFIG_DIR, keyid: DEFAULT_KEYID, passphrase: DEFAULT_PASSPHRASE, shell: EXEC_SHELL } );
+  const keyid = await gpg.generateKey( DEFAULT_PASSPHRASE, "lock-me-out-test" );
+  DEFAULT_KEYID = keyid;
+  gpg.configure( { keyid, configDir: CONFIG_DIR, passphrase: DEFAULT_PASSPHRASE, shell: EXEC_SHELL } ); // With updated keyid
+} )
+
+test( "gpg works", () => {
   gpg.version().
     then( version => {
       expect( version ).not.toBe( null ); // Extend error message
@@ -15,5 +32,4 @@ test( "configure gpg", () => {
         expect( Number( version[0] ) ).toBeGreaterThan( 1 );
     } )
 } );
-
 
--- test/unit/db/index.test.ts
+++
@@ -1,6 +0,0 @@
-import db from "../../../src/db/index";
-
-test( "db has Sequelize obj", () => {
-  expect( db.Sequelize ).not.toBe( undefined );
-} );
-
